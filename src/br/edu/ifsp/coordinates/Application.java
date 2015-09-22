c1b140a320f3150d8cd7627b7c6b228c663754ed
Matheus da Silva Ferreira <matheus.dasilva.ferreira@hotmail.com>
2015-09-22 13:54:47 -0300

    Created the class Coordinate. This class get the joints of the users tracked during the execution and store their values in a Map.

--- src/br/edu/ifsp/coordinates/Application.java
+++ src/br/edu/ifsp/coordinates/Application.java
@@ -1,14 +1,66 @@
 package br.edu.ifsp.coordinates;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.openni.Device;
 import org.openni.DeviceInfo;
 import org.openni.OpenNI;
 
+import com.primesense.nite.JointType;
+import com.primesense.nite.NiTE;
+import com.primesense.nite.SkeletonJoint;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
 public class Application {
 
-	public static void main(String args[]){
+	public static void main(String args[]) {
+		Load.load();
 		
+		// java.util.ConcurrentModificationException
+		// Map<Short, List<float[][]>> usersCoordinates = new ConcurrentHashMap<>();
+		Map<Short, List<float[][]>> usersCoordinates = new HashMap<>();
+		Coordinate coor = new Coordinate(usersCoordinates);
+		Thread t = new Thread(coor);
+		t.start();
+
+		try {
+			Thread.sleep(7_000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		coor.stopDetectingFrames();
+		NiTE.shutdown();
+		OpenNI.shutdown();
+
+		Set<Short> keys = usersCoordinates.keySet();
+		for (Short key : keys) {
+			imprimir(usersCoordinates, key);
+		}
+		
+	}
+	
+	public static void imprimir(Map<Short, List<float[][]>> usersCoordinates, Short key){
+		List<float[][]> moves = usersCoordinates.get(key);
+		System.out.println("User " + key + " in " + moves.size() + " frames detected.");
+		float[][][] teste = moves.toArray(new float[moves.size()][JointType.values().length][3]);
+
+		for (int h = 0; h < teste.length; h++) {
+			System.out.print(h);
+			float[][] fs = teste[h];
+			
+			for (int i = 0; i < fs.length; i++) {
+				float[] value = fs[i];
+				System.out.print(Arrays.toString(value));
+			}
+			System.out.println("");
+		}
 	}
 }
---
+++ src/br/edu/ifsp/coordinates/Coordinate.java
@@ -0,0 +1,95 @@
+package br.edu.ifsp.coordinates;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import com.primesense.nite.JointType;
+import com.primesense.nite.Skeleton;
+import com.primesense.nite.SkeletonJoint;
+import com.primesense.nite.SkeletonState;
+import com.primesense.nite.UserData;
+import com.primesense.nite.UserTracker;
+import com.primesense.nite.UserTrackerFrameRef;;
+
+public class Coordinate implements Runnable, UserTracker.NewFrameListener {
+
+	public static final int X = 0, Y = 1, Z = 2;
+	private UserTracker userTracker = null;
+	private Map<Short, List<float[][]>> coordinates;
+
+	public Coordinate(UserTracker userTracker, Map<Short, List<float[][]>> coordinates) {
+		this.userTracker = userTracker;
+		this.coordinates = coordinates;
+	}
+
+	public Coordinate(Map<Short, List<float[][]>> coordinates) {
+		userTracker = UserTracker.create();
+		this.coordinates = coordinates;
+	}
+
+	@Override
+	public void onNewFrame(UserTracker userTracker) {
+		UserTrackerFrameRef frame = userTracker.readFrame();
+
+		List<UserData> users = frame.getUsers();
+		for (UserData user : users) {
+			if (user.isLost()) {
+				System.out.println("User " + user.getId() + " was lost.");
+				continue;
+			}
+			if (user.isNew()) {
+				System.out.println("User " + user.getId() + " found. Starting tracking.");
+				userTracker.startSkeletonTracking(user.getId());
+				continue;
+			}
+			if(!user.getSkeleton().getState().equals(SkeletonState.TRACKED)){
+				System.out.println("User " + user.getId() + " not tracked yet.");
+				continue;
+			}
+
+			float[][] joints = tracking(user);
+			List<float[][]> userMoves = coordinates.get(user.getId());
+
+			if (userMoves == null) {
+				//userMoves = new CopyOnWriteArrayList<>();
+				userMoves = new ArrayList<>();
+				coordinates.put(user.getId(), userMoves);
+			}
+
+			userMoves.add(joints);
+		}
+		
+		frame.release();
+	}
+
+	@Override
+	public void run() {
+		userTracker.addNewFrameListener(this);
+	}
+
+	private float[][] tracking(UserData user) {
+		Skeleton skeleton = user.getSkeleton();
+		JointType[] jointTypes = JointType.values();
+		float[][] joints = new float[jointTypes.length][3];
+
+		for (int i = 0; i < jointTypes.length; i++) {
+			SkeletonJoint joint = skeleton.getJoint(jointTypes[i]);
+			joints[i][X] = joint.getPosition().getX();
+			joints[i][Y] = joint.getPosition().getY();
+			joints[i][Z] = joint.getPosition().getZ();
+		}
+		
+		return joints;
+	}
+	
+	public void stopDetectingFrames(){
+		userTracker.removeNewFrameListener(this);
+	}
+	
+	public Map<Short, List<float[][]>> getCoordinates(){
+		return coordinates;
+	}
+
+}
