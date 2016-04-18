4c206f9a9fe8a42abdeb56a2477976d512b59005
Matheus da Silva Ferreira <matheus.dasilva.ferreira@hotmail.com>
2016-04-18 17:36:05 -0300

    The class responsible for saving a ByteBuffer object in an external file in realtime.

---
+++ src/br/edu/ifsp/util/SaveOnline.java
@@ -0,0 +1,38 @@
+package br.edu.ifsp.util;
+
+import java.io.File;
+import java.nio.ByteBuffer;
+
+import br.edu.ifsp.util.Save;
+
+public class SaveOnline implements Runnable {
+
+	public static int COUNT = 0;
+
+	private File file;
+	private ByteBuffer frame;
+
+	private Save save = new Save();
+
+	public SaveOnline(File file, ByteBuffer frame) {
+		this.file = file;
+		this.frame = frame;
+	}
+
+	@Override
+	public void run() {
+		save.saveBuffer(file, frame);
+
+		COUNT++;
+		if (COUNT % 500 == 0) {
+			callGC();
+		}
+	}
+
+	private synchronized void callGC() {
+		Runtime run = Runtime.getRuntime();
+		run.runFinalization();
+		run.gc();
+
+	}
+}
